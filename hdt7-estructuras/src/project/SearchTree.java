93b415ed185fb2acb5053a49bb4b633151da557f
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-03-25 21:31:19 -0600

    no tengo tiempo para esto :)

--- hdt7-estructuras/src/project/SearchTree.java
+++ hdt7-estructuras/src/project/SearchTree.java
@@ -1,5 +1,6 @@
 package project;
 
+import java.security.Key;
 import java.util.Comparator;
 
 public class SearchTree <K,V> {
@@ -9,5 +10,14 @@ public class SearchTree <K,V> {
 	
 	public SearchTree(Comparator<K> KeyComparator) {
 		this.KeyComparator = KeyComparator;
+		root = null;
+		count = 0;
+	}
+	
+	public V internalSearch(Node<K,V> toSearch, K id) {
+		if (toSearch != null) {
+			int resutl = KeyComparator.compare(toSearch.getKey(), id);
+		}
+		return null;
 	}
 }
